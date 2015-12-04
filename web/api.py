e87fa6284afda10baafc0e6be5dae0747528813c
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-12-04 21:28:27 +0900

    http basic auth working on index

--- web/api.py
+++ web/api.py
@@ -34,6 +34,7 @@ from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy, event
 
 flapp.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://halfdan:halfdan@localhost/techrice'
+flapp.config['SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(flapp)
 
@@ -197,13 +198,47 @@ class Reading(ExtendedBase, db.Model):
 		return {'id': self.id, 'sensor_id': self.sensor_id, 'value': self.value, 'timestamp': str(self.timestamp), 'created': str(self.created), 'updated': str(self.updated)}
 
 
+############################################################
+######## SECURITY STUFF
+############################################################
+
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(flapp, user_datastore)
 
 
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='dalekk@gmail.com', password='password')
+    db.session.commit()
 
 
-
-
-
+# Views
+@flapp.route('/')
+@login_required
+def home():
+    return 'hello'
 
 
 
