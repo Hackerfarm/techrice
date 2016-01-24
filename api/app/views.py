28a5749db83e4ea50e39ae122d5b081ff663130f
Halfdan Rump <halfdan-rump@garage.co.jp>
2016-01-24 20:30:49 +0900

    Adds GET /seed/node/techrice

--- api/app/views.py
+++ api/app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask.ext.security import login_required
-from flask import request
+from flask import request, jsonify
 
 
 # @app.route('/', methods = ['GET'])
@@ -15,3 +15,11 @@ def unauthorized():
 @app.route('/posttest', methods = ['POST'])
 def posttest():
 	return str(request.form)
+
+from flask.ext.security import http_auth_required
+import seed
+@app.route('/seed/node/techrice')
+@http_auth_required
+def seed_techrice_node():
+	return jsonify(seed.seed_techrice_node())
+
