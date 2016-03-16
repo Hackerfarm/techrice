5577ef40483a670097dfb15fa010bb715fefeda5
Halfdan Rump <halfdan-rump@garage.co.jp>
2016-03-16 21:30:31 +0900

    Adds time convert function

--- devices/arashiv2/sketches/techrice_edge/techrice_edge.ino
+++ devices/arashiv2/sketches/techrice_edge/techrice_edge.ino
@@ -1,26 +1,10 @@
-/*
-  Web client
-
- This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe, based on work by Adrian McEwen
-
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <chibi.h>
 //#include "TimerOne.h"
 #define RX_BUFSIZE 300
 
-#define NODE_ID 3
+#define NODE_ID 1
 
 unsigned char buf[RX_BUFSIZE];
 int len;
@@ -29,8 +13,8 @@ unsigned char old[100];
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-char server[] = "iotree.org";
-const int port = 10000;
+char server[] = "api.techrice.jp";
+const int port = 80;
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 1, random(100,200));
@@ -39,7 +23,7 @@ IPAddress ip(192, 168, 1, random(100,200));
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
-
+EthernetUDP udp;
 
 /*
 This struct will be common for all nodes
@@ -65,6 +49,22 @@ typedef struct{
 } techrice_packet_t;
 
 
+#include <avr/time.h>
+
+const int _ytab[2][12] = {
+                { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+                { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+        };
+
+#define YEAR0           2000                    /* the first year */
+#define EPOCH_YR        1970            /* EPOCH = Jan 1 1970 00:00:00 */
+#define SECS_DAY        (24L * 60L * 60L)
+#define LEAPYEAR(year)  (!((year) % 4) && (((year) % 100) || !((year) % 400)))
+#define YEARSIZE(year)  (LEAPYEAR(year) ? 366 : 365)
+#define FIRSTSUNDAY(timp)       (((timp)->tm_yday - (timp)->tm_wday + 420) % 7)
+#define FIRSTDAYOF(timp)        (((timp)->tm_wday - (timp)->tm_yday + 420) % 7)
+#define TIME_MAX        ULONG_MAX
+#define ABB_LEN         3
 
 
 
@@ -91,6 +91,25 @@ void setup() {
 
   // give the Ethernet shield a second to initialize:
   delay(1000);
+  Serial.println("Getting time");
+  unsigned long unixTime = ntpUnixTime(udp);
+  Serial.println(unixTime);
+
+  time_t rawtime = unixTime;
+  struct tm *info;
+  /* Get GMT time */
+  info = gmtime(&rawtime );
+  //printf("time: %d\n", rawtime);
+
+  Serial.println(info->tm_year);
+  Serial.println(info->tm_mon);
+  Serial.println(info->tm_mday);
+  Serial.println(info->tm_hour);
+  Serial.println(info->tm_min);
+  Serial.println(info->tm_sec);
+
+
+
   Serial.println("Started");
 }
 
@@ -129,7 +148,7 @@ void loop()
 
 void api_post(char *http_body){
   char http_header[400];
-  sprintf(http_header, "POST /readings HTTP/1.1\r\nHost: techrice.iotree.org\r\nAuthorization: Basic dGVjaHJpY2VAaGFja2VyLmZhcm06dW5peHRoZWdyZWF0\r\nContent-Length: %d\r\nUser-Agent: arashi2\r\nConnection: close\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", strlen(http_body));
+  sprintf(http_header, "POST /readings HTTP/1.1\r\nHost: api.techrice.jp\r\nAuthorization: Basic dGVjaHJpY2VAaGFja2VyLmZhcm06dW5peHRoZWdyZWF0\r\nContent-Length: %d\r\nUser-Agent: arashi2\r\nConnection: close\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", strlen(http_body));
   strcat(http_header, http_body);
   Serial.println("WHOLE REQUEST");
   Serial.println(http_header);
@@ -146,6 +165,113 @@ void api_post(char *http_body){
     Serial.println("connection failed");
     client.stop();
   }
+}
+
+
+
+/*
+ * © Francesco Potortì 2013 - GPLv3 - Revision: 1.13
+ *
+ * Send an NTP packet and wait for the response, return the Unix time
+ *
+ * To lower the memory footprint, no buffers are allocated for sending
+ * and receiving the NTP packets.  Four bytes of memory are allocated
+ * for transmision, the rest is random garbage collected from the data
+ * memory segment, and the received packet is read one byte at a time.
+ * The Unix time is returned, that is, seconds from 1970-01-01T00:00.
+ */
+unsigned long ntpUnixTime (UDP &udp)
+{
+  static int udpInited = udp.begin(123); // open socket on arbitrary port
+
+  const char timeServer[] = "pool.ntp.org";  // NTP server
+
+  // Only the first four bytes of an outgoing NTP packet need to be set
+  // appropriately, the rest can be whatever.
+  const long ntpFirstFourBytes = 0xEC0600E3; // NTP request header
+
+  // Fail if WiFiUdp.begin() could not init a socket
+  if (! udpInited)
+    return 0;
+
+  // Clear received data from possible stray received packets
+  udp.flush();
+
+  // Send an NTP request
+  if (! (udp.beginPacket(timeServer, 123) // 123 is the NTP port
+   && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
+   && udp.endPacket()))
+    return 0;       // sending request failed
+
+  // Wait for response; check every pollIntv ms up to maxPoll times
+  const int pollIntv = 150;   // poll every this many ms
+  const byte maxPoll = 15;    // poll up to this many times
+  int pktLen;       // received packet length
+  for (byte i=0; i<maxPoll; i++) {
+    if ((pktLen = udp.parsePacket()) == 48)
+      break;
+    delay(pollIntv);
+  }
+  if (pktLen != 48)
+    return 0;       // no correct packet received
+
+  // Read and discard the first useless bytes
+  // Set useless to 32 for speed; set to 40 for accuracy.
+  const byte useless = 40;
+  for (byte i = 0; i < useless; ++i)
+    udp.read();
+
+  // Read the integer part of sending time
+  unsigned long time = udp.read();  // NTP time
+  for (byte i = 1; i < 4; i++)
+    time = time << 8 | udp.read();
+
+  // Round to the nearest second if we want accuracy
+  // The fractionary part is the next byte divided by 256: if it is
+  // greater than 500ms we round to the next second; we also account
+  // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
+  // additionally, we account for how much we delayed reading the packet
+  // since its arrival, which we assume on average to be pollIntv/2.
+  time += (udp.read() > 115 - pollIntv/8);
+
+  // Discard the rest of the packet
+  udp.flush();
+
+  return time - 2208988800ul;   // convert NTP time to Unix time
+}
+
+
+struct tm *
+gmtime(register const time_t *timer)
+{
+        static struct tm br_time;
+        register struct tm *timep = &br_time;
+        time_t time = *timer;
+        register unsigned long dayclock, dayno;
+        int year = EPOCH_YR;
+
+        dayclock = (unsigned long)time % SECS_DAY;
+        dayno = (unsigned long)time / SECS_DAY;
+
+        timep->tm_sec = dayclock % 60;
+        timep->tm_min = (dayclock % 3600) / 60;
+        timep->tm_hour = dayclock / 3600;
+        timep->tm_wday = (dayno + 4) % 7;       /* day 0 was a thursday */
+        while (dayno >= YEARSIZE(year)) {
+                dayno -= YEARSIZE(year);
+                year++;
+        }
+        timep->tm_year = year - YEAR0;
+        timep->tm_yday = dayno;
+        timep->tm_mon = 0;
+        while (dayno >= _ytab[LEAPYEAR(year)][timep->tm_mon]) {
+                dayno -= _ytab[LEAPYEAR(year)][timep->tm_mon];
+                timep->tm_mon++;
+        }
+        timep->tm_mday = dayno + 1;
+        timep->tm_isdst = 0;
+
+        return timep;
 }
 
 
