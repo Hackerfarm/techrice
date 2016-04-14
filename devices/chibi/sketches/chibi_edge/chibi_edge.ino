6e6eb502724bfef5ee9ac2504d4df11b49c8d844
Halfdan Rump <halfdan-rump@garage.co.jp>
2016-04-14 16:26:00 +0900

    basic mechanisms for node/edge request/response time exhchange in place

--- devices/chibi/sketches/chibi_edge/chibi_edge.ino
+++ devices/chibi/sketches/chibi_edge/chibi_edge.ino
@@ -32,18 +32,18 @@ unsigned char old[100];
 This struct will be common for all nodes
 */
 
-#define TIME_PACKET 0
-#define TECHRICE_PACKET 1
-
-// typedef struct{
-//   int8_t type;
-//   char *payload;
-// } packet_t;
+#define CURRENT_TIME_REQUEST 0
+#define CURRENT_TIME_RESPONSE 1
+#define TECHRICE_PACKET 2
 
 typedef struct{
   int8_t type;
   char payload[80];
 } packet_t;
+
+typedef struct{
+  uint32_t year;
+} datetime_t;
 
 
 typedef struct{
@@ -90,44 +90,89 @@ void setup() {
 
 void loop()
 {
-
   if (chibiDataRcvd() == true)
   { 
+    
     int rssi, src_addr;
     len = chibiGetData(buf);
     if (len == 0) return;
-    
+
     // retrieve the data and the signal strength
     rssi = chibiGetRSSI();
     src_addr = chibiGetSrcAddr();
-    Serial.println(len);
-    Serial.print("Signal strength: ");
-    Serial.println(rssi);
+    
+
+    
     if (len)
     {
-      
-      packet_t packet = *((packet_t*)(buf));
-      Serial.print("Packet type: ");
-      Serial.println(packet.type);
-      techrice_packet_t p = *((techrice_packet_t*)(packet.payload));
-      Serial.print("payload size: ");
-      Serial.println(sizeof(packet.payload));
-      // Serial.print("data:");
-      // Serial.println(packet.payload);
-      p.signal_strength = rssi;
-      p.node_id = NODE_ID;
-      char http_body[300];
+      packet_t request = *((packet_t*)(buf));
+      print_packet_info(len, src_addr, rssi, request);
 
-    sprintf(http_body, "format=compact&readings=%d,%d;%d,%d;%d,%d;%d,%d;%d,%d",
-                (int) p.temperature.sensor_id, (int) p.temperature.value,
-                (int) p.humidity.sensor_id, (int) p.humidity.value,
-                (int) p.battery.sensor_id, (int) p.battery.value,
-                (int) p.solar.sensor_id, (int) p.solar.value,
-                (int) p.sonar.sensor_id, (int) p.sonar.value);
-    Serial.print("data: ");
-    Serial.println(http_body);
 
+      if(request.type == CURRENT_TIME_REQUEST){
+        
+        // packet_t response;
+        // response.type = CURRENT_TIME_RESPONSE;
+        datetime_t now = {2016};
+        // memcpy(response.payload, &now, sizeof(now));
+
+        packet_t response = init_packet(CURRENT_TIME_RESPONSE, &now);
+        chibiTx(src_addr, (uint8_t*)(&response), sizeof(response));
+        Serial.println("Response sent");
+      } else if (request.type == TECHRICE_PACKET){
+        
+        techrice_packet_t p = *((techrice_packet_t*)(request.payload));
+
+        // Serial.print("data:");
+        // Serial.println(packet.payload);
+        p.signal_strength = rssi;
+        p.node_id = NODE_ID;
+        char http_body[300];
+
+        sprintf(http_body, "format=compact&readings=%d,%d;%d,%d;%d,%d;%d,%d;%d,%d",
+                  (int) p.temperature.sensor_id, (int) p.temperature.value,
+                  (int) p.humidity.sensor_id, (int) p.humidity.value,
+                  (int) p.battery.sensor_id, (int) p.battery.value,
+                  (int) p.solar.sensor_id, (int) p.solar.value,
+                  (int) p.sonar.sensor_id, (int) p.sonar.value);
+        Serial.print("data: ");
+        Serial.println(http_body);
+      }
     }
   }
 
 }
+
+void print_packet_info(int len, int src_addr, int rssi, packet_t packet){
+  Serial.println();
+  Serial.print(millis());
+  Serial.print(", Packet from: ");
+  Serial.print(src_addr);
+  Serial.print(", len: ");
+  Serial.print(len);
+  Serial.print(", signal strength: ");
+  Serial.print(rssi);
+  Serial.print(" , packet type: ");
+  Serial.print(packet.type);
+  Serial.print(", payload size: ");
+  Serial.println(sizeof(packet.payload));  
+}
+
+packet_t init_packet(int type, void *payload){
+  packet_t packet;
+  packet.type = type;
+  switch (type) {
+      case CURRENT_TIME_REQUEST:
+        break; // no payload to be copied
+      case CURRENT_TIME_RESPONSE:
+        memcpy(packet.payload, payload, sizeof(datetime_t));
+        break;
+      case TECHRICE_PACKET:
+        memcpy(packet.payload, payload, sizeof(techrice_packet_t));
+        break;
+      default:
+        break;
+  }
+  return packet;
+}
+
--- devices/chibi/sketches/chibi_node/chibi_node.ino
+++ devices/chibi/sketches/chibi_node/chibi_node.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <stdint.h>
-
+#include <avr/time.h>
 
 
 /*
@@ -10,19 +10,25 @@ START OF API-GENERATED HEADER
 */
 
 
-#define TIME_PACKET 0
-#define TECHRICE_PACKET 1
+#define CURRENT_TIME_REQUEST 0
+#define CURRENT_TIME_RESPONSE 1
+#define TECHRICE_PACKET 2
 
 typedef struct{
   int8_t type;
   char payload[80];
 } packet_t;
 
+typedef struct{
+  uint32_t year;
+} datetime_t;
+
 
 typedef struct{
   int32_t sensor_id;
   int32_t value;
 } reading_t;
+
 
 
 
@@ -74,18 +80,6 @@ END OF OF API-GENERATED HEADER
 #define DATECODE "08-09-2014"
 #define ADCREFVOLTAGE 3.3
 
-int hgmPin = 22;
-int sdCsPin = 15;
-int rtcCsPin = 28; 
-int ledPin = 18;
-int sdDetectPin = 19;
-int vbatPin = 31;
-int vsolPin = 29;
-int sensorPin = 8;
-int sonarTriggerPin = 5;
-int sonarEchoPin = 7;
-int sonarAwakePin = 10;
-int burstModePin = 4;
 
 
 // Pins that will not interfer with the SPI: 2 to 5, 7 to 10 + 14 and 15
@@ -104,6 +98,9 @@ void setup()
   chibiCmdInit(57600);
   // Initialize the chibi wireless stack
   chibiInit();
+  request_time();
+  
+
 }
 
 /**************************************************************************/
@@ -113,27 +110,8 @@ void loop()
 {
   static int count = 0;
   
-  // This function checks the command line to see if anything new was typed.
-//  chibiCmdPoll();
-
-  digitalWrite(sonarAwakePin, HIGH);
-
-  /*if(r.sonar.value>10){
-    digitalWrite(sonarAwakePin, LOW);
-  }
-  else{
-    digitalWrite(sonarAwakePin, HIGH);
-  }*/
   r.count++;
-  // get_timestamp(r.timestamp);
-
-  
-  // packet_t packet = {TECHRICE_PACKET, (char*) &r};
-
-
   char sbuf[SBUF_SIZE];
-  sprintf(sbuf, "Port: %d Bit: %d",digitalPinToPort(hgmPin), digitalPinToBitMask(hgmPin));
-  Serial.println(sbuf);
 
 //  r.timestamp = "2016-04-14 10:35:00";
   r.temperature.value = 28;
@@ -141,7 +119,7 @@ void loop()
   r.vbat.value = 4200;
   r.vsol.value = 6000;
   
-  sprintf(sbuf, "Node_id: %d, cozunt: %d, timestamp: %19s, id %d: %dC (temperature), id %d: %d (humidity), id %d: %dmV (battery), id %d: %dmV (solar), id %d: %d cm (water level)", 
+  sprintf(sbuf, "Node_id: %d, count: %d, timestamp: %19s, id %d: %dC (temperature), id %d: %d (humidity), id %d: %dmV (battery), id %d: %dmV (solar), id %d: %d cm (water level)", 
                 (int) r.node_id,
                 (int) r.count, 
                 (int) r.timestamp,
@@ -152,19 +130,123 @@ void loop()
                 (int) r.distance_to_water_surface.sensor_id, (int) r.distance_to_water_surface.value);
   Serial.println(sbuf);
 
-  packet_t packet;
-  packet.type = TECHRICE_PACKET;
-  memcpy(packet.payload, &r, sizeof(r));
+  // packet_t request;
+  // request.type = TECHRICE_PACKET;
+  // memcpy(request.payload, &r, sizeof(r));
+
+  packet_t request = init_packet(TECHRICE_PACKET, &r);
+
    // = {TECHRICE_PACKET, (char*) &r};
-  // packet_t packet = {TECHRICE_PACKET, "Hello"};
+  // packet_t request = {TECHRICE_PACKET, "Hello"};
   Serial.print("payload size: ");
-  Serial.println(sizeof(packet.payload));
-  chibiTx(EDGE_ID, (unsigned char*)(&packet), sizeof(packet));
+  Serial.println(sizeof(request.payload));
+  chibiTx(EDGE_ID, (unsigned char*)(&request), sizeof(request));
   free(sbuf);
   delay(5000);
 }
 
 
 
+int request_time(){
+  Serial.println("Sending request for current time");
+  packet_t request;
+  request.type = CURRENT_TIME_REQUEST;
+  chibiTx(EDGE_ID, (uint8_t*)(&request), sizeof(request));
+  free(&request);
+
+  while(!chibiDataRcvd()); // Wait forever until response
+  
+  int rssi, src_addr;
+  int len = chibiGetData(buf);
+  if (len == 0) return 0;
+
+  // retrieve the data and the signal strength
+  rssi = chibiGetRSSI();
+  src_addr = chibiGetSrcAddr();
+  
+  packet_t response = *((packet_t*)(buf));
+  print_packet_info(len, src_addr, rssi, response);
+
+  datetime_t now = *((datetime_t*)(response.payload));
+  
+
+  Serial.print("time now: ");
+  Serial.println(now.year);
+  // while(true);
+  if (len){  
+    packet_t packet = *((packet_t*)(buf));
+    if(!(packet.type == CURRENT_TIME_RESPONSE)){
+      Serial.print("Expected CURRENT_TIME_RESPONSE, but got ");        
+      Serial.println(packet.type);
+      return 0;
+    } 
+    datetime_t now = *((datetime_t*)(packet.payload));
+    Serial.print("time now: ");
+    Serial.println(now.year);
+  }
 
 
+
+
+
+
+  //   Serial.print("Packet type: ");
+  //   Serial.println(packet.type);
+  //   techrice_packet_t p = *((techrice_packet_t*)(packet.payload));
+  //   Serial.print("payload size: ");
+  //   Serial.println(sizeof(packet.payload));
+  //   // Serial.print("data:");
+  //   // Serial.println(packet.payload);
+  //   p.signal_strength = rssi;
+  //   p.node_id = NODE_ID;
+  //   char http_body[300];
+
+  // sprintf(http_body, "format=compact&readings=%d,%d;%d,%d;%d,%d;%d,%d;%d,%d",
+  //             (int) p.temperature.sensor_id, (int) p.temperature.value,
+  //             (int) p.humidity.sensor_id, (int) p.humidity.value,
+  //             (int) p.battery.sensor_id, (int) p.battery.value,
+  //             (int) p.solar.sensor_id, (int) p.solar.value,
+  //             (int) p.sonar.sensor_id, (int) p.sonar.value);
+  // Serial.print("data: ");
+  // Serial.println(http_body);
+
+//   }
+
+
+
+}
+
+void print_packet_info(int len, int src_addr, int rssi, packet_t packet){
+  Serial.println();
+  Serial.print(millis());
+  Serial.print(", Packet from: ");
+  Serial.print(src_addr);
+  Serial.print(", len: ");
+  Serial.print(len);
+  Serial.print(", signal strength: ");
+  Serial.print(rssi);
+  Serial.print(" , packet type: ");
+  Serial.print(packet.type);
+  Serial.print(", payload size: ");
+  Serial.println(sizeof(packet.payload));  
+}
+
+
+
+packet_t init_packet(int type, void *payload){
+  packet_t packet;
+  packet.type = type;
+  switch (type) {
+      case CURRENT_TIME_REQUEST:
+        break; // no payload to be copied
+      case CURRENT_TIME_RESPONSE:
+        memcpy(packet.payload, payload, sizeof(datetime_t));
+        break;
+      case TECHRICE_PACKET:
+        memcpy(packet.payload, payload, sizeof(techrice_packet_t));
+        break;
+      default:
+        break;
+  }
+  return packet;
+}
