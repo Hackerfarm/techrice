7e4f8a7942193077d3de0740839d3ca7b8f52b5d
Halfdan Rump <halfdan-rump@garage.co.jp>
2016-04-18 16:23:18 +0900

    Edge now converts integer timestamp to tm time struct and sends this to node

--- devices/chibi/sketches/chibi_edge/chibi_edge.ino
+++ devices/chibi/sketches/chibi_edge/chibi_edge.ino
@@ -69,6 +69,26 @@ typedef struct{
 
 
 
+#include <avr/time.h>
+
+const int _ytab[2][12] = {
+                { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+                { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+        };
+
+#define YEAR0           2000                    /* the first year */
+#define EPOCH_YR        1970            /* EPOCH = Jan 1 1970 00:00:00 */
+#define SECS_DAY        (24L * 60L * 60L)
+#define LEAPYEAR(year)  (!((year) % 4) && (((year) % 100) || !((year) % 400)))
+#define YEARSIZE(year)  (LEAPYEAR(year) ? 366 : 365)
+#define FIRSTSUNDAY(timp)       (((timp)->tm_yday - (timp)->tm_wday + 420) % 7)
+#define FIRSTDAYOF(timp)        (((timp)->tm_wday - (timp)->tm_yday + 420) % 7)
+#define TIME_MAX        ULONG_MAX
+#define ABB_LEN         3
+
+
+
+
 
 
 
@@ -94,26 +114,38 @@ void loop()
   { 
     int rssi, src_addr;
     len = chibiGetData(buf);
-    if (len == 0) return;
+    if (len == 0) {
+      Serial.println("LEN 0");
+      // return;
+    }
 
     // retrieve the data and the signal strength
-    rssi = chibiGetRSSI();
-    src_addr = chibiGetSrcAddr();
+    
         
     if (len)
     {
+      rssi = chibiGetRSSI();
+      src_addr = chibiGetSrcAddr();
+      Serial.println("GOT PACKET");
       packet_t request = *((packet_t*)(buf));
       print_packet_info(len, src_addr, rssi, request);
 
       switch (request.type) {
           case CURRENT_TIME_REQUEST:{
-              datetime_t now = {2016};
-              packet_t response = init_packet(CURRENT_TIME_RESPONSE, &now);
+              Serial.println("CURRENT_TIME_REQUEST received");
+              struct tm *now;
+              time_t unixtime = 1460956224;
+              now = gmtime(&unixtime);
+              Serial.println(now->tm_year);
+              Serial.println(now->tm_mon);
+              Serial.println(now->tm_mday);
+              packet_t response = init_packet(CURRENT_TIME_RESPONSE, now);
               chibiTx(src_addr, (uint8_t*)(&response), sizeof(response));
-              Serial.println("Response sent");
+              Serial.println("CURRENT_TIME_REQUEST response sent");
             }
             break;
           case TECHRICE_PACKET: {
+              Serial.println("TECHRICE_PACKET received");
               techrice_packet_t p = *((techrice_packet_t*)(request.payload));
               p.signal_strength = rssi;
               p.node_id = src_addr;
@@ -127,6 +159,7 @@ void loop()
                         (int) p.sonar.sensor_id, (int) p.sonar.value);
               Serial.print("data: ");
               Serial.println(http_body);
+
             }
             break;
           default:
@@ -136,6 +169,41 @@ void loop()
   }
 
 }
+
+struct tm *
+gmtime(register const time_t *timer)
+{
+        static struct tm br_time;
+        register struct tm *timep = &br_time;
+        time_t time = *timer;
+        register unsigned long dayclock, dayno;
+        int year = EPOCH_YR;
+
+        dayclock = (unsigned long)time % SECS_DAY;
+        dayno = (unsigned long)time / SECS_DAY;
+
+        timep->tm_sec = dayclock % 60;
+        timep->tm_min = (dayclock % 3600) / 60;
+        timep->tm_hour = dayclock / 3600;
+        timep->tm_wday = (dayno + 4) % 7;       /* day 0 was a thursday */
+        while (dayno >= YEARSIZE(year)) {
+                dayno -= YEARSIZE(year);
+                year++;
+        }
+        timep->tm_year = year - YEAR0;
+        timep->tm_yday = dayno;
+        timep->tm_mon = 0;
+        while (dayno >= _ytab[LEAPYEAR(year)][timep->tm_mon]) {
+                dayno -= _ytab[LEAPYEAR(year)][timep->tm_mon];
+                timep->tm_mon++;
+        }
+        timep->tm_mday = dayno + 1;
+        timep->tm_isdst = 0;
+
+        return timep;
+}
+
+
 
 void print_packet_info(int len, int src_addr, int rssi, packet_t packet){
   Serial.println();
@@ -159,7 +227,7 @@ packet_t init_packet(int type, void *payload){
       case CURRENT_TIME_REQUEST:
         break; // no payload to be copied
       case CURRENT_TIME_RESPONSE:
-        memcpy(packet.payload, payload, sizeof(datetime_t));
+        memcpy(packet.payload, payload, sizeof(tm));
         break;
       case TECHRICE_PACKET:
         memcpy(packet.payload, payload, sizeof(techrice_packet_t));
